e8e209c16f4f286dc1874d9fd23a2c8d4a916260
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-06-26 10:06:09 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const fs = require('fs')
+
+/**
+ * @param {String} filePath 
+ * @returns file, as promise
+ */
+const getFile = (filePath) => 
+    new Promise ((resolve, reject) => {
+       fs.readFile(filePath, (err, file) => {
+           if (err) throw err
+
+           resolve(file)
+       })
+    })
+
+/**
+ * @param {Array} words 
+ * @returns word at random index
+ */
+const pickRandom = words => words[
+    Math.floor(
+        Math.random() * words.length
+    )
+]
+
+;(async () => {
+    const file = 
+        await getFile('words.json')    
+    const words = JSON.parse(file)
+
+    const start = words.subjects
+    const mid = words.verbs
+    const end = words.objects
+        .concat(start)
+
+    setInterval(() => {
+        console.log(
+            `${pickRandom(start)} ${pickRandom(mid)} ${pickRandom(end)}`
+        )
+    }, 1000)
+})()
